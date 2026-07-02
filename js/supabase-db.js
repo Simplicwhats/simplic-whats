484bad113c5e59226615944802b1f2fcf7b876a3
Simplicwhats <levisd2104@gmail.com>
2026-07-02 13:10:39 -0300

    Update supabase-db.js

--- js/supabase-db.js
+++ js/supabase-db.js
@@ -1,12 +1,11 @@
 const SUPABASE_URL = "https://kgidkxaqvgcqiqwqxvut.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnaWRreGFxdmdjcWlxd3F4dnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODc0MzAsImV4cCI6MjA5ODA2MzQzMH0.DLQoO8_q_QeW-a084ZDCFRc0OIeuEDaYpkUg2tSCB0E";
 
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-let contatos = [];
-let whatsappAccounts = [];
-let listaScripts = [];
-let contaEditandoIndex = null;
-let usuarioLogado = "";
-let abaWhatsAppReferencia = null;
-let previewTimerInterval = null;
+window.contatos = [];
+window.whatsappAccounts = [];
+window.listaScripts = [];
+window.usuarioLogado = "";
+window.abaWhatsAppReferencia = null;
+window.previewTimerInterval = null;
